fa41e94e5c10d87f3c828e66c0c751b9b2508dce
saeed <fakhraei.saeed@gmail.com>
2023-02-13 13:20:25 +0000

    Spring Boot Initial Project

---
+++ src/main/java/com/saeedsanslimits/findidealweather/FindIdealWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.saeedsanslimits.findidealweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindIdealWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindIdealWeatherApplication.class, args);
+    }
+
+}
